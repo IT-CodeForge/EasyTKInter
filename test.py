50bd6554a8513aff2198b7b8d89b6085233e5d44
svn <svn>
2024-04-28 15:47:15 +0000

    -

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from ETK import *
 
+
 class GUI(ETKMainWindow):
     def __init__(self) -> None:
         super().__init__(caption="NENENE", background_color=0xFF0000)
@@ -8,8 +9,12 @@ class GUI(ETKMainWindow):
         pass
 
     def _add_elements(self) -> None:
-        self.label = ETKLabel(self._tk_object, "LABEL")
-        self.button = ETKButton(self._tk_object)
+        self.container = ETKContainer(
+            self._tk_object, size=ETKContainerSize(100, 100), outline_color=0x0000FF)
+        self.container.outline_color = 0x0000FF
+        # self.label = ETKLabel(self._tk_object)
+        # self.container.add_element(self.label, ETKAlignments.TOP_RIGHT)
+        # self.label.pos = vector2d(-10, 0)
 
 
 if __name__ == '__main__':
