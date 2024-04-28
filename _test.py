4da7a0906d51b10d4a65b3f0b4d26ea34ef33a82
svn <svn>
2024-04-28 15:05:19 +0000

    -

--- _test.py
+++
@@ -1,7 +0,0 @@
-from ETK.vector2d import vector2d
-from ast import literal_eval
-from random   import randint
-from time     import perf_counter
-
-if __name__ == "__main__":
-    print(tuple([1,2]))
