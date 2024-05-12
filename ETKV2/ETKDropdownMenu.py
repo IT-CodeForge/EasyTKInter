2d7aa7b802ec4243a5dd30bb70cdce24a21cef1b
svn <svn>
2024-05-12 16:09:04 +0000

    -

--- ETKV2/ETKDropdownMenu.py
+++ ETKV2/ETKDropdownMenu.py
@@ -14,6 +14,7 @@ class ETKDropdownMenu(ETKBaseTkWidgetDisableable):
     def __init__(self, tk: Tk, options: list[str], start_value: str = "", pos: vector2d = vector2d(0, 0), size: vector2d = vector2d(70, 18), background_color: int = 0xEEEEEE, **kwargs: Any) -> None:
         self.__selected = StringVar(value=start_value)
         self._tk_object = OptionMenu(tk, self.__selected, *options)
+        self.__ignore_next_change_event = False
 
         super().__init__(pos=pos, size=size, background_color=background_color, **kwargs)
 
@@ -22,8 +23,16 @@ class ETKDropdownMenu(ETKBaseTkWidgetDisableable):
 
     @property
     def selected(self) -> str:
-        """READ-Only"""
         return self.__selected.get()
+    
+    @selected.setter
+    def selected(self, value: str) -> None:
+        self.__ignore_next_change_event = True
+        self.__selected.set(value)
 
     def __clicked_changed(self, *args: str) -> None:
-        self._handle_event(ETKDropdownMenuEvents.CHANGED)
+        if not self.__ignore_next_change_event:
+            self._handle_event(ETKDropdownMenuEvents.CHANGED)
+        else:
+            self.__ignore_next_change_event = False
+
